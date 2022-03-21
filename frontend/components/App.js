28f7984fef3f015a8d92edde8badaa8c5574ba17
Ladrillo <gabriel@cabrejas.net>
2022-03-21 17:52:47 +0100

    committing to "lecture"

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -39,7 +39,7 @@ const inputChange = ({ name, value }) => {
 
 // 3- REDUCER is a function that takes current state and an action object
 // and returns the next state of the app
-const reducer = (state, action) => { // the action will look like { type: 'INPUT_CHANGE', payload: { 'name''foo' } }
+const reducer = (state, action) => { // the action will look like { type: 'INPUT_CHANGE', payload: { name: 'name' value: 'foo' } }
   switch (action.type) {
     case TOGGLE_DISPLAY_COMPLETEDS: {
       return { ...state, displayCompleteds: !state.displayCompleteds }
