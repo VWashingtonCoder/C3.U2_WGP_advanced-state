aafe5ed8fb947360a81eaf8f2b90495e2157acac
Ladrillo <gabriel@cabrejas.net>
2022-03-21 17:21:29 +0100

    committing to "lecture"

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -23,6 +23,7 @@ const initialState = {
 
 // a type of action that might change application state
 const INPUT_CHANGE = 'INPUT_CHANGE'
+const ADD_NEW_TODO = 'ADD_NEW_TODO'
 const 
 
 export default function App() {
