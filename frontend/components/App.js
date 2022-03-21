2afbf7de1d723ad8e4ac05f9cec2f232e70aa630
Ladrillo <gabriel@cabrejas.net>
2022-03-21 18:25:06 +0100

    committing to "lecture"

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -39,7 +39,7 @@ const inputChange = ({ name, value }) => {
   return { type: INPUT_CHANGE, payload: { name, value } }
 }
 const addNewTodo = (todoName) => {
-  return { type: ADD_NEW_TODO, payload: }
+  return { type: ADD_NEW_TODO, payload: { name: todoName, id: } }
 }
 
 // 3- REDUCER is a function that takes current state and an action object
@@ -70,8 +70,8 @@ export default function App() {
     // dispatch({ type: 'INPUT_CHANGE', payload: { name, value }})
   }
   const onSubmit = evt => {
-    evt.preventDefault()state.form.name
-    dispatch(addNewTodo())
+    evt.preventDefault()
+    dispatch(addNewTodo(state.form.name))
   }
   const toggleShouldShow = () => {
     dispatch(toggleDisplayCompleteds())
