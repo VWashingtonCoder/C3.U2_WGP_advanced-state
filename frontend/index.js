dbfeb7f11c83db9e359cca9200b5d7dd92e4860f
Andre Blaze <kuraiokamiblaze@gmail.com>
2022-03-22 13:59:05 -0600

    addd day 2 index.js

--- frontend/index.js
+++ frontend/index.js
@@ -3,11 +3,21 @@ import { render } from 'react-dom'
 import App from './components/App'
 import './styles/reset.css'
 import './styles/styles.css'
+// redux imports
+import { createStore, compose } from "redux"
+import { Provider } from "react-redux"
+import reducer from './state/reducer'
+
+// there is a million ways to make the store. this is just one.
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(reducer, composeEnhancers())
 
 render(
   <React.StrictMode>
-    <h1>Todo App</h1>
-    <App />
+    <Provider store={store}>
+      <h1>Todo App</h1>
+      <App />
+    </Provider>
   </React.StrictMode>
   , document.getElementById('root')
 )
